308570a9a2c96f059b8faf22f909197e36962b83
ClintonBeyene <clintonbeyene52@gmail.com>
2025-08-09 22:46:22 +0300

    chore(next): track Next.js env types (auto-generated; do not edit)

--- frontend/next-env.d.ts
+++ frontend/next-env.d.ts
@@ -3,3 +3,6 @@
 
 // NOTE: This file should not be edited
 // see https://nextjs.org/docs/basic-features/typescript for more information.
+
+// c:\Users\Hp\Documents\Programming\Hackathon\CubeSat_ImageClassify_ET_Group_6\cubesat\Scripts\python.exe -m uvicorn sagemaker_deploy.app.main:app --host 0.0.0.0 --port 8000
+
